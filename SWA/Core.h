7738e28e2f45ab58694daaaa3bfaefae4cb24265
Tom Roozen <taamrooz@avans.nl>
2019-09-18 22:21:59 +0200

    cmake with vcpkg toolchain

---
+++ SWA/Core.h
@@ -0,0 +1,8 @@
+#pragma once
+class Core
+{
+	void init(const char*, int, int, bool);
+	void input();
+	void render();
+};
+
--- SWA/main.cpp
+++ SWA/main.cpp
@@ -1,6 +1,8 @@
+#define SDL_MAIN_HANDLED
 #include "SDL.h"
 
-int main(int argc, char* argv[])
+
+int main(int argc, const char* argv[])
 {
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -13,15 +15,24 @@ int main(int argc, char* argv[])
 		0
 	);
 
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	SDL_SetRenderDrawColor(renderer, 0, 100, 0, SDL_ALPHA_OPAQUE);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
-
-	SDL_Delay(3000);
-
+	auto quit = false;
+	SDL_Event e;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e) != 0)
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+		SDL_UpdateWindowSurface(window);
+	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return 0;
 }
