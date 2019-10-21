83932585097be303ef044d6e6755d366d6eba6a6
Renato Feroce <rferoce@avans.nl>
2019-10-21 13:17:49 +0200

    Fixed speed when moving in oblique direction

--- SWA/MoveCharacterSystem.cpp
+++ SWA/MoveCharacterSystem.cpp
@@ -16,30 +16,59 @@ void MoveCharacterSystem::update(double dt) {
 	auto position = manager_->get_component<PositionComponent>(entity);
 	auto animation = manager_->get_component<AnimationComponent>(entity);
 
-	//Go through possible actions and check if any need to be executed
+	int counter = 0;
+
 	for (auto& i : KeyBindingSingleton::get_instance()->keys_down)
 	{
 		if (i.first == KeyBindingSingleton::get_instance()->get_move_up_key_binding()) {
 			if (i.second) {
-				velocity->dy = -1 * move_velocity;
+				counter++;
 			}
 		}
 
 		if (i.first == KeyBindingSingleton::get_instance()->get_move_left_key_binding()) {
 			if (i.second) {
-				velocity->dx = -1 * move_velocity;
+				counter++;
 			}
 		}
 
-		if (i.first == KeyBindingSingleton::get_instance()->get_move_up_key_binding()) {
+		if (i.first == KeyBindingSingleton::get_instance()->get_move_down_key_binding()) {
 			if (i.second) {
-				velocity->dy = move_velocity;
+				counter++;
 			}
 		}
 
 		if (i.first == KeyBindingSingleton::get_instance()->get_move_right_key_binding()) {
 			if (i.second) {
-				velocity->dx = move_velocity;
+				counter++;
+			}
+		}
+	}
+
+	//Go through possible actions and check if any need to be executed
+	for (auto& i : KeyBindingSingleton::get_instance()->keys_down)
+	{
+		if (i.first == KeyBindingSingleton::get_instance()->get_move_up_key_binding()) {
+			if (i.second) {
+				velocity->dy = (counter > 1) ? velocity->dy = -1 * (move_velocity / 2) : velocity->dy = -1 * move_velocity;
+			}
+		}
+
+		if (i.first == KeyBindingSingleton::get_instance()->get_move_left_key_binding()) {
+			if (i.second) {
+				velocity->dx = (counter > 1) ? velocity->dx = -1 * (move_velocity / 2) : velocity->dx = -1 * move_velocity;
+			}
+		}
+
+		if (i.first == KeyBindingSingleton::get_instance()->get_move_down_key_binding()) {
+			if (i.second) {
+				velocity->dy = (counter > 1) ? velocity->dy = move_velocity / 2 : velocity->dy = move_velocity;
+			}
+		}
+
+		if (i.first == KeyBindingSingleton::get_instance()->get_move_right_key_binding()) {
+			if (i.second) {
+				velocity->dx = (counter > 1) ? velocity->dx = move_velocity / 2 : velocity->dx = move_velocity;
 			}
 		}
 	}
