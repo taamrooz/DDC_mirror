8d398ffab7c8a7920502ca92c8a389f587674682
Mart van den Berg <mberg15@avans.nl>
2019-11-12 20:43:23 +0100

    handle damage in separate function

--- SWA/CollisionHandlers.cpp
+++ SWA/CollisionHandlers.cpp
@@ -1,10 +1,14 @@
 #include "CollisionHandlers.h"
-#include "BaseSystem.h"
-#include "CharacterComponent.h"
-#include "DamagingComponent.h"
-#include "HealthComponent.h"
-#include <Renderer.h>
-#include "AnimationComponent.h"
+
+void DamageHandler(HealthComponent* health, DamagingComponent* dmg) {
+
+	int currentTick = Engine::GetTicks();
+	if (health->invulnerable_until < currentTick) {
+		std::cout << "HIT" << std::endl;
+		health->current_health -= dmg->damage_amount;
+		health->invulnerable_until = health->time_invulnerable + currentTick;
+	}
+}
 
 void BulletCollisionHandler(uint32_t entity1, uint32_t entity2, EntityManager* manager)
 {
@@ -16,21 +20,18 @@ void BulletCollisionHandler(uint32_t entity1, uint32_t entity2, EntityManager* m
 
 void PlayerCollisionHandler(uint32_t entity1, uint32_t entity2, EntityManager* manager)
 {
+	
 	auto dmg = manager->get_component<DamagingComponent>(entity2);
 	if (dmg != nullptr) {
-		int currentTick = Engine::GetTicks();
+		auto ani = manager->get_component<AnimationComponent>(entity1);
+		ani->currentState = State::HIT;
+		ani->lock_until = Engine::GetTicks() + 250;
+
 		auto health = manager->get_component<HealthComponent>(entity1);
-		if (health->invulnerable_until < currentTick) {
-			std::cout << "HIT" << std::endl;
-			health->current_health -= dmg->damage_amount;
-			health->invulnerable_until = health->time_invulnerable + Engine::GetTicks();
-			auto ani = manager->get_component<AnimationComponent>(entity1);
-			ani->currentState = State::HIT;
-			ani->lock_until = currentTick + 250;
-			if (health->current_health <= 0) {
-				std::cout << "Game Over!" << std::endl;
-			}
-		}
+		DamageHandler(health, dmg);
 		
+		if (health->current_health <= 0) {
+			std::cout << "Game Over!" << std::endl;
+		}
 	}
 }
