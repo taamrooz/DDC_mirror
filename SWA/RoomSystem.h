2a2efbf351215910c1f9aa73ed9b4f5db371d39f
Marquatoes <33327930+Marquatoes@users.noreply.github.com>
2019-11-24 14:47:22 +0100

    Collision for walls
    Correct tiles now have collision, map is way more interesting to play ;)

--- SWA/RoomSystem.h
+++ SWA/RoomSystem.h
@@ -15,7 +15,7 @@ private:
 	const int k_total_tiles_ = 300;
 	const int k_tile_height_ = 64;
 	const int k_tile_width_ = 64;
-	const int k_total_sprites_ = 36;
-	const int k_collision_tiles[8] = {23, 34, 27, 06, 12, 19, 16, 02};
+	const int k_total_sprites_ = 49;
+	const int k_collision_tiles[37] = { 2, 3, 4, 5 ,6 ,9 ,11 ,12 , 13 ,16, 17, 18 ,19 ,20 ,23 ,24 , 25 ,26 ,27 ,30 ,31 ,32 ,33 ,34, 36 ,37 ,38 ,39 ,40, 41, 42, 43, 44, 45, 46, 47, 48};
 };
 
