509ab7c6cff74de05b4c8598b64074d774d86218
Marquatoes <33327930+Marquatoes@users.noreply.github.com>
2019-11-30 15:28:18 +0100

    update

--- SWA/RoomSystem.h
+++ SWA/RoomSystem.h
@@ -11,11 +11,10 @@ public:
 	void update(double dt) override;
 private:
 	//Constants for the drawing of tiles
-	const int k_level_width_ = 1280;
-	const int k_total_tiles_ = 300;
-	const int k_tile_height_ = 64;
-	const int k_tile_width_ = 64;
-	const int k_total_sprites_ = 49;
-	const int k_collision_tiles[37] = { 2, 3, 4, 5 ,6 ,9 ,11 ,12 , 13 ,16, 17, 18 ,19 ,20 ,23 ,24 , 25 ,26 ,27 ,30 ,31 ,32 ,33 ,34, 36 ,37 ,38 ,39 ,40, 41, 42, 43, 44, 45, 46, 47, 48};
+	constexpr static int k_level_width_ = 1280;
+	constexpr static int k_total_tiles_ = 300;
+	constexpr static int k_tile_height_ = 64;
+	constexpr static int k_tile_width_ = 64;
+	constexpr static int k_total_sprites_ = 49;
+	constexpr static int k_collision_tiles[37] = { 2, 3, 4, 5 ,6 ,9 ,11 ,12 , 13 ,16, 17, 18 ,19 ,20 ,23 ,24 , 25 ,26 ,27 ,30 ,31 ,32 ,33 ,34, 36 ,37 ,38 ,39 ,40, 41, 42, 43, 44, 45, 46, 47, 48};
 };
-
