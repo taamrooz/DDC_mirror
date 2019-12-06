bd0a1ec35bc82f35b4bd0ea10b8be3afd3a6e170
Mart van den Berg <mberg15@avans.nl>
2019-12-06 23:30:13 +0100

    progress wall avoidance

--- SWA/MoveSystem.cpp
+++ SWA/MoveSystem.cpp
@@ -58,7 +58,7 @@ void MoveSystem::update(double dt)
 	double dist = 1000000000.0;
 	vector2d shortest = vector2d();
 	PositionComponent tilePos;
-	vector2d enemyVec = normalize(vector2d(vel->dx, vel->dy));
+	vector2d enemyVec = normalize(vel->steer_force);
 	for (auto const& node_tuple : collisions) {
 		Engine::Node* first_node = std::get<0>(node_tuple);
 		Engine::Node* second_node = std::get<1>(node_tuple);
@@ -70,7 +70,7 @@ void MoveSystem::update(double dt)
 			if (enemyComponent != nullptr && tileComponent != nullptr) {
 				auto wallPos = manager_->get_component<PositionComponent>(second_node->id);
 				auto enemyPos = manager_->get_component<PositionComponent>(first_node->id);
-				
+
 				auto enemyColl = manager_->get_component<CollisionComponent>(first_node->id);
 				auto playerPos = manager_->get_component<PositionComponent>(player.front());
 
@@ -78,17 +78,21 @@ void MoveSystem::update(double dt)
 				vector2d vec2 = normalize(vector2d(wallPos->x - enemyPos->x, (wallPos->y + tileComponent->height) - enemyPos->y));
 				vector2d vec3 = normalize(vector2d(wallPos->x - enemyPos->x, wallPos->y - enemyPos->y));
 				vector2d vec4 = normalize(vector2d(wallPos->x + tileComponent->width - enemyPos->x, wallPos->y + tileComponent->height - enemyPos->y));
-				
+				Engine::render_line(wallPos->x + tileComponent->width, wallPos->y, wallPos->x, wallPos->y + tileComponent->height);
+				Engine::render_line(wallPos->x, wallPos->y, wallPos->x + tileComponent->width, wallPos->y + tileComponent->height);
 				Engine::set_render_draw_color(255, 255, 255, 255);
-				Engine::render_line(enemyPos->x, enemyPos->y, enemyPos->x + enemyVec.x() * 1000, enemyPos->y + enemyVec.y() * 1000);
 				Engine::set_render_draw_color(0, 255, 0, 255);
 
-				if ((enemyVec.x() <= vec1.x() && enemyVec.x() >= vec2.x() && enemyVec.y() >= vec1.y() && enemyVec.y() <= vec2.y()) || (enemyVec.x() >= vec3.x() && enemyVec.x() <= vec4.x() && enemyVec.y() >= vec3.y() && enemyVec.y() <= vec4.y())) {
+				if ((enemyVec.x() <= vec1.x() && enemyVec.x() >= vec2.x() && enemyVec.y() >= vec1.y() && enemyVec.y() <= vec2.y()) ||
+					(enemyVec.x() >= vec1.x() && enemyVec.x() <= vec2.x() && enemyVec.y() <= vec1.y() && enemyVec.y() >= vec2.y()) ||
+					(enemyVec.x() >= vec3.x() && enemyVec.x() <= vec4.x() && enemyVec.y() >= vec3.y() && enemyVec.y() <= vec4.y()) ||
+					(enemyVec.x() <= vec3.x() && enemyVec.x() >= vec4.x() && enemyVec.y() <= vec3.y() && enemyVec.y() >= vec4.y())) {
 					vector2d wallVector = vector2d(((enemyPos->x + enemyColl->width) - wallPos->x), 1);
 					if (length(vector2d(wallPos->x - enemyPos->x, wallPos->y - enemyPos->y)) < dist) {
 						dist = length(vector2d(wallPos->x - enemyPos->x, wallPos->y - enemyPos->y));
+
 						tilePos = *wallPos;
-						shortest = vec1;
+						shortest = vector2d(wallPos->x + tileComponent->width - enemyPos->x, wallPos->y + tileComponent->height - enemyPos->y);
 					}
 				}
 			}
@@ -97,7 +101,71 @@ void MoveSystem::update(double dt)
 
 	//Todo: calculate steer force
 	Engine::draw_rectangle(rect2d(tilePos.x, tilePos.y, 64, 64));
-	vel->dx += 0.3;
+	//vector2d desiredVel = perp(vel->steer_force);
+	auto pos = manager_->get_component<PositionComponent>(enemyEnt);
+	//if (pos->x > tilePos.x) {
+	//	desiredVel = vector2d(desiredVel.x() * -1, desiredVel.y());
+	//}
+	//if (pos->y > tilePos.y) {
+	//	desiredVel = vector2d(desiredVel.x(), desiredVel.y() * -1);
+	//}
+
+	//vel->steer_force += desiredVel;
+	vector2d overshoot = (normalize(vel->steer_force) * 150) - shortest;
+	//vector2d desiredForce = vector2d(tilePos.x, tilePos.y + 64 + (diff * -1));
+	//vector2d desiredVel = normalize(desiredForce - vector2d(pos->x, pos->y)) * 20;
+	vector2d target = vector2d();
+	double distance = 100000.0;
+	if (tilePos.x != 0) {
+		if (length(vector2d(tilePos.x + 32 - pos->x, tilePos.y - pos->y)) < distance) {
+			//top
+			distance = length(vector2d(tilePos.x + 32 - pos->x, tilePos.y - pos->y));
+			if (vel->steer_force.x() < 0) {
+				target = vector2d(length(vel->steer_force) * -1, 0);
+			}
+			else {
+				target = vector2d(length(vel->steer_force), 0);
+			}
+		}
+		if (length(vector2d(tilePos.x - pos->x, tilePos.y + 32 - pos->y)) < distance) {
+			//left
+			distance = length(vector2d(tilePos.x - pos->x, tilePos.y + 32 - pos->y));
+			if (vel->steer_force.y() < 0) {
+				target = vector2d(0, length(vel->steer_force) * -1);
+			}
+			else {
+				target = vector2d(0, length(vel->steer_force));
+			}
+		}
+		if (length(vector2d(tilePos.x + 64 - pos->x, tilePos.y + 32 - pos->y)) < distance) {
+			//right
+			distance = length(vector2d(tilePos.x + 64 - pos->x, tilePos.y + 32 - pos->y));
+
+			if (vel->steer_force.y() < 0) {
+				target = vector2d(0, length(vel->steer_force) * -1);
+			}
+			else {
+				target = vector2d(0, length(vel->steer_force));
+			}
+
+		}
+		if (length(vector2d(tilePos.x + 32 - pos->x, tilePos.y + 64 - pos->y)) < distance) {
+			//bot
+			distance = length(vector2d(tilePos.x + 32 - pos->x, tilePos.y + 64 - pos->y));
+
+			if (vel->steer_force.x() < 0) {
+				target = vector2d(length(vel->steer_force) * -1, 0);
+			}
+			else {
+				target = vector2d(length(vel->steer_force), 0);
+			}
+
+		}
+		vel->steer_force = target;
+	}
+
+	Engine::render_line(pos->x, pos->y, pos->x + vel->steer_force.x() * 100, pos->y + vel->steer_force.y() * 100);
+
 
 	if (!enemies.empty() && !player.empty()) {
 		for (auto enemyEntity : enemies)
@@ -106,7 +174,7 @@ void MoveSystem::update(double dt)
 			auto enemyMass = manager_->get_component<EnemyComponent>(enemyEntity);
 			auto vel = manager_->get_component<VelocityComponent>(enemyEntity);
 			auto pos = manager_->get_component<PositionComponent>(enemyEntity);
-			
+
 			const vector2d acceleration = vel->steer_force / enemyMass->mass;
 			vector2d velocity = vector2d(vel->dx, vel->dy);
 			velocity += acceleration * dt;
@@ -117,8 +185,9 @@ void MoveSystem::update(double dt)
 			if (velocity.y() > vel->maxSpeed) {
 				velocity = { velocity.y(), trunc(vel->maxSpeed) };
 			}
+
 			vel->dx = velocity.x();
-			
+
 			vel->dy = velocity.y();
 		}
 
