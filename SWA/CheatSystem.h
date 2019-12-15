d1ef1f44ac3bf8311c9c1efadc0533bce0ec67a9
Renato Feroce <rferoce@avans.nl>
2019-12-15 15:36:50 +0100

    fixed bug

--- SWA/CheatSystem.h
+++ SWA/CheatSystem.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "BaseSystem.h"
+#include "Core.h"
 class CheatSystem :
 	public BaseSystem
 {
 public:
-	CheatSystem(Engine::EntityManager<Component>* manager);
+	CheatSystem(Engine::EntityManager<Component>* manager, Core& core);
 	void update(double dt) override;
+private:
+	Core* core;
 };
 
